2284a55f8ea2acbaeae83939b314a08b21b1c0c8
Shivam Gupta <shivamrvgupta@gmail.com>
2023-02-20 01:32:28 +0530

    added loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-// loader
-
-var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
-  loader.style.display = "none";
-})
-
 (function() {
   "use strict";
 
